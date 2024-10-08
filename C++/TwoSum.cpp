2e17d62b50058e22a0c82521aa6619b6da21dfb4
Yogeshh <114099821+YogeshBabyOP@users.noreply.github.com>
2024-10-09 00:47:44 +0530

    Create TwoSum.cpp
    Added both Naive and Optimal Solutions

---
+++ C++/TwoSum.cpp
@@ -0,0 +1,32 @@
+//Brute Force
+class Solution {
+public:
+    vector<int> twoSum(vector<int> &nums, int target) {
+        for (int i = 0; i < nums.size(); i++) {
+            for (int j = i + 1; j < nums.size(); j++) {
+                if (nums[j] == target - nums[i]) {
+                    return {i, j};
+                }
+            }
+        }
+        return {};
+    }
+};
+
+// Optimal
+class Solution {
+public:
+    vector<int> twoSum(vector<int> &nums, int target) {
+        unordered_map<int, int> hash;
+        for (int i = 0; i < nums.size(); i++) {
+            hash[nums[i]] = i;
+        }
+        for (int i = 0; i < nums.size(); i++) {
+            int complement = target - nums[i];
+            if (hash.find(complement) != hash.end() && hash[complement] != i) {
+                return {i, hash[complement]};
+            }
+        }
+        return {};
+    }
+};
